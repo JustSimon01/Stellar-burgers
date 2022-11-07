d6316a7f5769364ce1e92f0a0a7fd89bd621987b
JustSimon01 <simon.ch@yandex.ru>
2022-11-08 00:41:42 +0300

    modal заказа + overlay

---
+++ src/components/ModalOverlay/ModalOverlay.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Modal from '../Modal/Modal';
+import modalClass from './ModalOverlay.css';
+
+
+function ModalOverlay() {
+  return (
+    <div className='ModalOverlay'>
+      <Modal />
+    </div>
+  );
+}
+
+export default ModalOverlay;
